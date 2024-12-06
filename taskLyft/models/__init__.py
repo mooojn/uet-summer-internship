45dedfe487165308aafdf542c6727b65ade0cc62
mooojn <moojntariq@gmail.com>
2024-12-06 18:25:57 +0500

    init models file

---
+++ taskLyft/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+from . import tasklyft_user
+from . import tasklyft_service
+from . import tasklyft_order_history
+from . import tasklyft_service_request
---
+++ taskLyft/models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class ./addons_project/task_lyft(models.Model):
+#     _name = './addons_project/task_lyft../addons_project/task_lyft'
+#     _description = './addons_project/task_lyft../addons_project/task_lyft'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+

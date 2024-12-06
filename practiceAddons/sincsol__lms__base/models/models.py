ef4ebef4a2fdcde1d3015d47ca73f4aac83b3e07
mooojn <moojntariq@gmail.com>
2024-12-06 18:31:43 +0500

    Create models.py

---
+++ practiceAddons/sincsol__lms__base/models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class ./addons_sincsol/sincsol__lms__base(models.Model):
+#     _name = './addons_sincsol/sincsol__lms__base../addons_sincsol/sincsol__lms__base'
+#     _description = './addons_sincsol/sincsol__lms__base../addons_sincsol/sincsol__lms__base'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+

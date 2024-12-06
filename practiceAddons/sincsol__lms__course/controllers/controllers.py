d489ed5afdf350e5e9350b0df3117a3c19178925
mooojn <moojntariq@gmail.com>
2024-12-06 18:34:46 +0500

    init files

---
+++ practiceAddons/sincsol__lms__course/controllers/controllers.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+
+
+# class ./addonsUstadam/sincsolLmsCourse(http.Controller):
+#     @http.route('/./addons_ustadam/sincsol__lms__course/./addons_ustadam/sincsol__lms__course', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/./addons_ustadam/sincsol__lms__course/./addons_ustadam/sincsol__lms__course/objects', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('./addons_ustadam/sincsol__lms__course.listing', {
+#             'root': '/./addons_ustadam/sincsol__lms__course/./addons_ustadam/sincsol__lms__course',
+#             'objects': http.request.env['./addons_ustadam/sincsol__lms__course../addons_ustadam/sincsol__lms__course'].search([]),
+#         })
+
+#     @http.route('/./addons_ustadam/sincsol__lms__course/./addons_ustadam/sincsol__lms__course/objects/<model("./addons_ustadam/sincsol__lms__course../addons_ustadam/sincsol__lms__course"):obj>', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('./addons_ustadam/sincsol__lms__course.object', {
+#             'object': obj
+#         })
+
---
+++ practiceAddons/sincsol__lms__course/models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class ./addons_ustadam/sincsol__lms__course(models.Model):
+#     _name = './addons_ustadam/sincsol__lms__course../addons_ustadam/sincsol__lms__course'
+#     _description = './addons_ustadam/sincsol__lms__course../addons_ustadam/sincsol__lms__course'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+

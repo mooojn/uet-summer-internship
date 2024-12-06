e82f81c8ade6bdc378ee83ea976ae182657e9128
mooojn <moojntariq@gmail.com>
2024-12-06 18:30:42 +0500

    init files

---
+++ practiceAddons/sincsol__lms__base/models/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+
+from . import user
+from . import student_result
+from . import certificate
+from . import question
+from . import option
+from . import course_content
+from . import course
+from . import quiz
